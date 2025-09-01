2ae6aabaca334a55259e9f0b437b01b5fd8a029d
Vitória Machado Estevão <144611699+VitoriaEstevao@users.noreply.github.com>
2025-09-01 08:21:22 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 $host = "ec2-184-72-188-139.compute-1.amazonaws.com";   // No AWS será o endpoint do RDS
 $user = "root";        // Usuário do MySQL
-$pass = "ifsp";            // Senha do MySQL
+$pass = "";            // Senha do MySQL
 $db   = "padaria";     // Nome do banco
 
 $conn = new mysqli($host, $user, $pass, $db);
